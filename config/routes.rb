3d55f6bc9d1161b4c1034dacccd2c886291b2604
rossmcarthur <r.mcarth20@gmail.com>
2018-03-13 17:08:08 -0400

    Backend auth setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "static_pages#root"
 
-  
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create, :destroy]
+  end
+  resource :session, only: [:create, :destroy, :show]
+
 end
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: './frontend/taskmaster.jsx',
+  entry: './frontend/chirp.jsx',
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
