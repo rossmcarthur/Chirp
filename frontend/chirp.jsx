6f8e6c980402a00b8de135ac7ae79561a672039b
rossmcarthur <r.mcarth20@gmail.com>
2018-03-13 17:44:19 -0400

    Test store in window

--- frontend/chirp.jsx
+++ frontend/chirp.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore();
   const root = document.getElementById('root');
+  window.store = store;
+  window.getState = store.getState;
   ReactDOM.render(<h1>Chirp!</h1>, root);
 });
