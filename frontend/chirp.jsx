caaa742ba27f2308a35e04d08cc242fd4964f931
rossmcarthur <r.mcarth20@gmail.com>
2018-03-13 17:50:04 -0400

    Refactor entry file"

--- frontend/chirp.jsx
+++ frontend/chirp.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let store = configureStore();
-  const root = document.getElementById('root');
+  const store = configureStore();
   window.store = store;
   window.getState = store.getState;
-  ReactDOM.render(<h1>Chirp!</h1>, root);
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 });
---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>Chirp! YEAH!</h1>
+  </div>
+);
+
+export default App;
