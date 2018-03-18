1276e3737e626fb97790bf01b5473c0de4459b94
rossmcarthur <r.mcarth20@gmail.com>
2018-03-18 16:21:23 -0400

    Remove first_name and last_name null constraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :first_name, :last_name, :email, :session_token, presence: true
+  validates :email, :session_token, presence: true
   validates :email, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313205655) do
+ActiveRecord::Schema.define(version: 20180318201943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "password_digest", null: false
     t.string "session_token", null: false
     t.string "location"
